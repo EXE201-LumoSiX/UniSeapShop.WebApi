4c9465adc78858e012da4f687778e6c96e060272
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2025-10-15 20:34:27 +0700

    feat: Add BlobController and update Supplier entity

--- UniSeapShop.Domain/Entities/Supplier.cs
+++ UniSeapShop.Domain/Entities/Supplier.cs
@@ -3,8 +3,8 @@ namespace UniSeapShop.Domain.Entities;
 public class Supplier : BaseEntity
 {
     public Guid UserId { get; set; }
-    public string Description { get; set; } = string.Empty;
-    public string Location { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public string? Location { get; set; } = string.Empty;
     public float Rating { get; set; } = 0;
     public bool IsActive { get; set; } = true;
 
---
+++ UniSeapShop.Domain/Migrations/20251015132202_supplier.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniSeapShop.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class supplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Rating",
+                table: "Suppliers",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Suppliers");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Rating",
+                table: "Suppliers",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldDefaultValue: 0f);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
